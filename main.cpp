9afb0243168de531fbeac675369814a349edb09d
Bibidh <080bct021.bibidh@pcampus.edu.np>
2025-01-23 23:28:52 +0545

    ->Fixed the Training Issue, now at least can 'Trainn' the network to learn the filters ->Realized I am shit at writing C++ -> Returning by reference to assign a new value is NOT a good idea -> I still do not know if the math works but we shall see

--- main.cpp
+++ main.cpp
@@ -93,6 +93,7 @@ int main()
 		// ------------------------------------------------------------------
 		// Apply pooling to the output features
 		// -----------------------------------------------------------------
+		l1.get_final_pool_maps().clear();
 		for (int i = 0; i < l1.get_output_feature_maps().size(); i++)
 		{
 			gridEntity pMap = l1.apply_pooling_univeral(l1.get_output_feature_maps().at(i), 2);
@@ -101,9 +102,7 @@ int main()
 		// ------------------------------------------------------------------
 		// Flatten the pooled layer
 		// -----------------------------------------------------------------
-		int chanels = l1.get_final_pool_maps().size(); // no. of filters
-		int filt_height = l1.get_final_pool_maps().at(0).size(); // rows
-		int filt_width = l1.get_final_pool_maps().at(0).at(0).size();
+		
 		std::vector<double> flatVec;
 		for (const auto& matrix : l1.get_final_pool_maps()) {
 			for (const auto& row : matrix) {
@@ -113,6 +112,10 @@ int main()
 		// ------------------------------------------------------------------
 		// Feed into deep neural network 
 		// -----------------------------------------------------------------
+		int chanels = l1.get_final_pool_maps().size(); // no. of filters
+		int filt_height = l1.get_final_pool_maps().at(0).size(); // rows
+		int filt_width = l1.get_final_pool_maps().at(0).at(0).size();
+		
 		vector<double> inputs = flatVec;
 		vector<double> target = { 0,1 };
 		double learning_rates = 0.01;
@@ -128,6 +131,7 @@ int main()
 		//-----------------------------------------------------------------------------------------------------
 		// Reshape the gradients in kernal form i.e. for not channels '3' ota widthxheight '11x11' ko array
 		//-----------------------------------------------------------------------------------------------------
+		
 		GeneralMatrix::Matrix* reqGrads = new GeneralMatrix::Matrix(1, flatVec.size(), false); 
 		GeneralMatrix::Matrix* last_gradiet = GradientMatrices.at(GradientMatrices.size() - 1);
 		GeneralMatrix::Matrix* tranposedWeightMatrices = Net->GetWeightMatrices().at(0)->tranpose();
@@ -158,29 +162,32 @@ int main()
 		//--------------------------------------------------------------------------------------------------------
 		// Unpool the gardients from their pooled map to get the gradients to the filter maps
 		//--------------------------------------------------------------------------------------------------------
+		
 		std::vector<gridEntity> filterMapGradients(l1.get_all_training_filter().size(),gridEntity(l1.get_output_feature_maps().at(0).size(), std::vector<double>(l1.get_output_feature_maps().at(0).at(0).size(), 0.0)));
+		
 		volumetricEntity unpooledGradients;
 		for (int ch = 0; ch < chanels; ch++)
 		{
-			std::cout << "  " << ch << std::endl;
 			gridEntity unpooled_map = l1.unpool_without_indices(poolGradients[ch], all_final_set_of_filter_maps[ch], 2,2,2);
 			unpooledGradients.push_back(unpooled_map);
 		}
+
+		
 		//-----------------------------------------------------------------------------------------------------
 		// Get the gradients for filters from the gradients to the filtermaps we just obtained
 		// -----------------------------------------------------------------------------------------------------
 		std::vector<gridEntity> inputChannels = l1.get_input_channels();
 		std::vector<volumetricEntity> filterGradients = l1.compute_filter_gradients(inputChannels,unpooledGradients,1);
+		
 		//-----------------------------------------------------------------------------------------------------
 		// Update the filters based on the computed Gradients
 		//-----------------------------------------------------------------------------------------------------
 		double learningRate = 0.01; // Example learning rate
 		l1.update_filters_with_gradients(l1.get_all_training_filter(),filterGradients,learningRate);
-
 		std::cout << "Network info: " << std::endl;
 		std::cout << "No. of feature maps from initial CL : "<<l1.get_feature_map().size() << std::endl;
 		std::cout << "No. of traniable filters : " << l1.get_all_training_filter().size() << std::endl;
-		std::cout << "Dimention of trainable filter :" <<l1.get_all_training_filter().at(0).size() << "x"<< l1.get_all_training_filter().at(0).at(0).size() <<std::endl;
+		std::cout << "Dimention of trainable filter :" <<l1.get_all_training_filter().at(0).size() << "x"<< l1.get_all_training_filter().at(0).at(0).size()<<"x"<< l1.get_all_training_filter().at(0).at(0).at(0).size() << std::endl;
 		std::cout << "No. of filters maps after application of trainable filters : " <<l1.get_output_feature_maps().size()<< std::endl;
 		std::cout << "Dimention of filter map after application of trainable filters : " << l1.get_output_feature_maps().at(0).size()<<"x"<< l1.get_output_feature_maps().at(0).at(0).size() << std::endl;
 		std::cout << "No. of pooled maps for those filter maps :" << l1.get_final_pool_maps().size() << std::endl;
